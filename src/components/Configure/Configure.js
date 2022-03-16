e0b3c3df3465bab3c3f1aefcbe2d174b01ee8714
sverrepaulsen <101713655+sverrepaulsen@users.noreply.github.com>
2022-03-16 15:42:34 +0100

    initial firebase setup

--- src/components/Configure/Configure.js
+++ src/components/Configure/Configure.js
@@ -1,9 +1,41 @@
-import * as React from "react";
+import React, {useState, useEffect, useRef} from "react";
+import db from  "../../app/firebase"
+import { collection, query, where, getDocs } from "firebase/firestore"; 
 
 export default function Configure() {
-  return (
-    <div>
-      <p> Sample Data </p>
-    </div>
+    const [blogs,setBlogs] = useState([])
+    const hasFetchedData = useRef(false);
+
+
+    useEffect(() => {
+        if(!hasFetchedData.current)
+        {
+            fetchBlogs();
+            hasFetchedData.current = true;
+        }
+        console.log(blogs)
+      }, )
+
+    const fetchBlogs = async () => {
+        const q = query(collection(db, "Blogs"), where("id", ">", 1)); //, where("capital", "==", true)
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            setBlogs(blogs => [...blogs, doc.data()])
+        });
+    }
+
+    return (
+        <div className="App">
+            {
+                blogs && blogs.map(blog => {
+                return(
+                    <div key={blog.id} className="blog-container">
+                        <h4>{blog.title}</h4>
+                        <p>{blog.body}</p>
+                    </div>
+                )
+                })
+            }
+        </div>
   );
 }
