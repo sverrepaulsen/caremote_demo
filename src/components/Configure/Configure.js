c9e83111f86621024bd95dadf614cf5c28a773ec
sverrepaulsen <101713655+sverrepaulsen@users.noreply.github.com>
2022-03-18 11:35:27 +0100

    initial query testing

--- src/components/Configure/Configure.js
+++ src/components/Configure/Configure.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect, useRef} from "react";
 import db from  "../../app/firebase"
 import { collection, query, where, getDocs } from "firebase/firestore"; 
+import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 export default function Configure() {
+    const navigate = useNavigate();
     const [blogs,setBlogs] = useState([])
     const hasFetchedData = useRef(false);
 
@@ -13,12 +16,16 @@ export default function Configure() {
             fetchBlogs();
             hasFetchedData.current = true;
         }
-        console.log(blogs)
       }, )
 
     const fetchBlogs = async () => {
-        const q = query(collection(db, "Blogs"), where("id", ">", 1)); //, where("capital", "==", true)
-        const querySnapshot = await getDocs(q);
+        const q = query(collection(db, "Blogs")); //, where("capital", "==", true)
+        const q1 = query(collection(db, "Blogs"), where("id", ">", 1));
+        const q2 = query(collection(db, "Blogs"), where("posted_by", "!=", "Paulsen"))
+        const q3 = query(collection(db, "Blogs"), where("contains", "array-contains", "image"))
+        const q4 = query(collection(db, "Blogs"), where("title", "in", ['Demo Blog', 'Demo 3']))
+        const q5 = query(collection(db, "Blogs"), where("posted_by", "==", "Sverre"), where("contains", "array-contains", "text"));
+        const querySnapshot = await getDocs(q5);
         querySnapshot.forEach((doc) => {
             setBlogs(blogs => [...blogs, doc.data()])
         });
@@ -36,6 +43,7 @@ export default function Configure() {
                 )
                 })
             }
+            <Button onClick={() => navigate("/")}>go back</Button>
         </div>
   );
 }
